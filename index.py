17245c1f6fbf7452d861eb8179b33809e0c2f526
Ritika ghimire <ghimireritikaa@gmail.com>
2026-02-13 09:49:12 +0545

    face detection

---
+++ index.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+
+img = cv.imread(r"IMG-20251026-WA0052.jpg")
+resized = cv.resize(img, (500, 500))
+gray = cv.cvtColor(resized,  cv.COLOR_BGR2GRAY)
+
+face_cascade = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_frontalface_default.xml")
+faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=8)
+
+for (x, y, w, h) in faces :
+    cv.rectangle(resized, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+cv.imshow("Faces", resized)
+cv.waitKey(0)
